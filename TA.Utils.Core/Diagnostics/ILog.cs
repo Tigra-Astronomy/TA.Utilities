e61ba3659ad0250c1ab90eb7d9367f53c3e92d1e
Tim Long <Tim@tigranetworks.co.uk>
2020-07-15 08:11:52 +0100

    Filled out Intellisense comments for logging

--- TA.Utils.Core/Diagnostics/ILog.cs
+++ TA.Utils.Core/Diagnostics/ILog.cs
@@ -14,18 +14,51 @@ using System.Runtime.CompilerServices;
 
 namespace TA.Utils.Core.Diagnostics
     {
+    /// <summary>
+    /// Logging service interface
+    /// </summary>
     public interface ILog
         {
+        /// <summary>
+        /// Creates a log builder for a log entry with Trace severity.
+        /// </summary>
+        /// <param name="callerFilePath">The caller file path.</param>
+        /// <returns>IFluentLogBuilder.</returns>
         IFluentLogBuilder Trace([CallerFilePath] string callerFilePath = null);
 
+        /// <summary>
+        /// Debugs the specified caller file path.
+        /// </summary>
+        /// <param name="callerFilePath">The caller file path.</param>
+        /// <returns>IFluentLogBuilder.</returns>
         IFluentLogBuilder Debug([CallerFilePath] string callerFilePath = null);
 
+        /// <summary>
+        /// Informations the specified caller file path.
+        /// </summary>
+        /// <param name="callerFilePath">The caller file path.</param>
+        /// <returns>IFluentLogBuilder.</returns>
         IFluentLogBuilder Info([CallerFilePath] string callerFilePath = null);
 
+        /// <summary>
+        /// Warnings the specified caller file path.
+        /// </summary>
+        /// <param name="callerFilePath">The caller file path.</param>
+        /// <returns>IFluentLogBuilder.</returns>
         IFluentLogBuilder Warning([CallerFilePath] string callerFilePath = null);
 
+        /// <summary>
+        /// Errors the specified caller file path.
+        /// </summary>
+        /// <param name="callerFilePath">The caller file path.</param>
+        /// <returns>IFluentLogBuilder.</returns>
         IFluentLogBuilder Error([CallerFilePath] string callerFilePath = null);
 
+        /// <summary>
+        /// Fatals the specified caller file path.
+        /// </summary>
+        /// <param name="callerFilePath">The caller file path.</param>
+        /// <returns>IFluentLogBuilder.</returns>
         IFluentLogBuilder Fatal([CallerFilePath] string callerFilePath = null);
         }
     }
